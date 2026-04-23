a82e55a02243faac22947192c59c84bb42009b15
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-23 16:55:04 +0300

    create user urls

--- tixora_backend/apps/users/urls.py
+++ tixora_backend/apps/users/urls.py
@@ -1,4 +1,23 @@
+# apps/users/urls.py
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-# This must be plural and exactly spelled like this
-urlpatterns = [] 
+from .views import (
+    TixoraLoginView,
+    RegisterView,
+    LogoutView,
+    ProfileView,
+    ChangePasswordView,
+)
+
+urlpatterns = [
+    # Auth
+    path('register/',        RegisterView.as_view(),       name='register'),
+    path('login/',           TixoraLoginView.as_view(),    name='login'),
+    path('logout/',          LogoutView.as_view(),         name='logout'),
+    path('token/refresh/',   TokenRefreshView.as_view(),   name='token_refresh'),
+
+    # Profile
+    path('profile/',         ProfileView.as_view(),        name='profile'),
+    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
+]
