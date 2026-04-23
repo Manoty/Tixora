f924e32cc0be148ab52253be08b3db22dce5fb4a
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-23 15:17:18 +0300

    Create Placeholder User Model

--- tixora_backend/apps/users/models.py
+++ tixora_backend/apps/users/models.py
@@ -1,3 +1,19 @@
+# apps/users/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'admin', 'Admin'
+        ORGANIZER = 'organizer', 'Organizer'
+        CUSTOMER = 'customer', 'Customer'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CUSTOMER
+    )
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
